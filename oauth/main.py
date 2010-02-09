f594cbedb0c1107c04faddc57ad92a2c68150dca
unknown <Eric@.(none)>
2010-02-08 19:35:34 -0800

    test python oauth on appengine initial import.

---
+++ oauth/main.py
@@ -0,0 +1,368 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	 http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import oauth
+import hashlib
+import binascii
+import string
+from time import time
+
+import logging
+
+from google.appengine.ext import db
+from google.appengine.api import urlfetch
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+from datastore import *
+
+REALM = 'http://eyuentest.appspot.com'
+
+# base clase to be extended by other handlers needing oauth
+class BaseHandler(webapp.RequestHandler):
+	def __init__(self, *args, **kwargs):
+		self.oauth_server = oauth.OAuthServer(DataStore())
+		self.oauth_server.add_signature_method(oauth.OAuthSignatureMethod_PLAINTEXT())
+		self.oauth_server.add_signature_method(oauth.OAuthSignatureMethod_HMAC_SHA1())
+	# end __init__ method
+
+	def send_oauth_error(self, err=None):
+		self.response.clear()
+		header = oauth.build_authenticate_header(realm=REALM)
+		for k,v in header.iteritems():
+			self.response.headers.add_header(k, v)
+		self.response.set_status(401, str(err.message))
+		logging.error(err.message)
+	# end send_oauth_error method
+
+	def get(self):
+		self.handler()
+	# end get method
+
+	def post(self):
+		self.handler()
+	# end post method
+
+	def handler(self):
+		pass
+	# end handler method
+
+	# get the request
+	def construct_request(self, token_type = ''):
+		logging.debug('\n\n' + token_type + 'Token------')
+		logging.debug(self.request.method)
+		logging.debug(self.request.url)
+		logging.debug(self.request.headers)
+
+		# get any extra parameters required by server
+		self.paramdict = {}
+		for j in self.request.arguments():
+			self.paramdict[j] = self.request.get(j)
+		logging.debug(self.paramdict)
+
+		# construct the oauth request from the request parameters
+		try:
+			oauth_request = oauth.OAuthRequest.from_request(self.request.method, self.request.url, headers=self.request.headers, parameters=self.paramdict)
+			return oauth_request
+		except oauthOauthError, err:
+			self.send_oauth_error(oauth.OAuthError('could not create oauth_request'))
+			self.send_oauth_error(err)
+			return False
+
+		# extra check... 
+		if not oauth_request:
+			self.send_oauth_error(oauth.OAuthError('could not create oauth_request'))
+			return False
+# End BaseHandler class
+
+
+class RequestTokenHandler(BaseHandler):
+	def handler(self):
+		oauth_request = self.construct_request('Request')
+		if not oauth_request:
+			self.send_oauth_error(oauth.OAuthError('could not create oauth_request'))
+			return
+
+		try:
+			token = self.oauth_server.fetch_request_token(oauth_request)
+
+			self.response.out.write(token.to_string())
+			logging.debug('Request Token created')
+		except oauth.OAuthError, err:
+			self.send_oauth_error(err)
+	# end handler method
+# End RequestTokenHandler class
+
+# required fields: 
+# - username: string 
+# - password: string, sha1 of plaintext password
+class UserAuthorize(BaseHandler):
+	def handler(self):
+		oauth_request = self.construct_request('Authorize')
+		if not oauth_request:
+			self.send_oauth_error(oauth.OAuthError('could not create oauth_request'))
+			return
+	
+		try:
+			# set by construct_request
+			if 'username' in self.paramdict:
+				username = self.paramdict['username']
+			if 'password' in self.paramdict:
+				password = self.paramdict['password']
+
+			if not username or not password:
+				self.response.set_status(401, 'missing username or password')
+				logging.error('missing username or password')
+				return
+
+			ukey = UserTable().check_valid_password(username, password)
+
+			if not ukey:
+				self.response.set_status(401, 'incorrect username or password')
+				logging.error('incorrect username or password')
+				return
+
+			# perform user authorize
+			token = self.oauth_server.fetch_request_token(oauth_request)
+			token = self.oauth_server.authorize_token(token, ukey)
+			logging.debug(token)
+
+			logging.debug(token.to_string())
+
+			self.response.out.write(token.get_callback_url())
+			logging.debug(token.get_callback_url())
+		except oauth.OAuthError, err:
+			self.send_oauth_error(err)
+	# end handler method
+# End UserAuthorize Class
+
+class AccessTokenHandler(BaseHandler):
+	def handler(self):
+		oauth_request = self.construct_request('Access')
+		if not oauth_request:
+			self.send_oauth_error(oauth.OAuthError('could not create oauth_request'))
+			return
+
+		try:
+			token = self.oauth_server.fetch_access_token(oauth_request)
+
+			self.response.out.write(token.to_string())
+		except oauth.OAuthError, err:
+			self.send_oauth_error(err)
+	# end handler method
+# End AccessTokenHandler Class
+
+# res1
+class ProtectedResourceHandler(BaseHandler):
+	def handler(self):
+		oauth_request = self.construct_request('Protected')
+		if not oauth_request:
+			self.send_oauth_error(oauth.OAuthError('could not create oauth_request'))
+			return
+
+		logging.debug(oauth_request.parameters)
+		try:
+			consumer, token, params = self.oauth_server.verify_request(oauth_request)
+
+			if not ResourceTable().check_valid_consumer('res1', consumer.key):
+				self.send_oauth_error(oauth.OAuthError('consumer may not access this resource'))
+				return
+
+			self.response.out.write(UserTable().get_username(token.user) + ': <br />')
+			self.response.out.write(str(params))
+		except oauth.OAuthError, err:
+			self.send_oauth_error(err)
+	# end handler method
+# End ProtectedResourceHandler Class
+
+# res2
+class ProtectedResource2Handler(BaseHandler):
+	def handler(self):
+		oauth_request = self.construct_request('Protected')
+		if not oauth_request:
+			self.send_oauth_error(oauth.OAuthError('could not create oauth_request'))
+			return
+
+		logging.debug(oauth_request.parameters)
+		try:
+			consumer, token, params = self.oauth_server.verify_request(oauth_request)
+
+			if not ResourceTable().check_valid_consumer('res2', consumer.key):
+				self.send_oauth_error(oauth.OAuthError('consumer may not access this resource'))
+				return
+
+			self.response.out.write(UserTable().get_username(token.user) + ': <br />')
+			self.response.out.write(str(params))
+		except oauth.OAuthError, err:
+			self.send_oauth_error(err)
+	# end handler method
+# End ProtectedResourceHandler Class
+
+# res3
+class ProtectedResource3Handler(BaseHandler):
+	def handler(self):
+		oauth_request = self.construct_request('Protected')
+		if not oauth_request:
+			self.send_oauth_error(oauth.OAuthError('could not create oauth_request'))
+			return
+
+		logging.debug(oauth_request.parameters)
+		try:
+			consumer, token, params = self.oauth_server.verify_request(oauth_request)
+
+			if not ResourceTable().check_valid_consumer('res3', consumer.key):
+				self.send_oauth_error(oauth.OAuthError('consumer may not access this resource'))
+				return
+
+			self.response.out.write(UserTable().get_username(token.user) + ': <br />')
+			self.response.out.write(str(params))
+		except oauth.OAuthError, err:
+			self.send_oauth_error(err)
+	# end handler method
+# End ProtectedResourceHandler Class
+
+class CreateConsumer(webapp.RequestHandler):
+	def get(self):
+		self.handle()
+	def post(self):
+		self.handle()
+	def handle(self):
+		self.response.out.write('''
+<html>
+<body>
+<form action="/get_consumer" METHOD="POST">
+	Select resources:
+	resource 1 (read test): <input name="res1" type="checkbox" value="res1"><br />
+	resource 2 (write test): <input name="res2" type="checkbox" value="res2"><br />
+	resource 3 (some other resource): <input name="res3" type="checkbox" value="res3"><br />
+	<input type="submit" name="submitted">
+</form>
+</body>
+</html>
+		''')
+# End CreateConsumer class
+
+class GetConsumer(webapp.RequestHandler):
+	def get(self):
+		self.handle()
+	def post(self):
+		self.handle()
+	def handle(self):
+		if not self.request.get('submitted'):
+			self.response.out.write('no')
+			return
+
+		allowed_res = ['res1', 'res2', 'res3']
+		consumer = Consumer().insert_consumer('tester1')
+		self.response.out.write('key: '+consumer.key+'<br />\n')
+		self.response.out.write('pass: '+consumer.secret+'<br />\n')
+
+		if self.request.get('res1') in allowed_res:
+			if ResourceTable().create_resource(self.request.get('res1'), consumer.key):
+				self.response.out.write('has permission on res 1<br />')
+			else:
+				self.response.out.write('could not grant on res 1<br />')
+		if self.request.get('res2') in allowed_res:
+			if ResourceTable().create_resource(self.request.get('res2'), consumer.key):
+				self.response.out.write('has permission on res 2<br />')
+			else:
+				self.response.out.write('could not grant on res 2<br />')
+		if self.request.get('res3') in allowed_res:
+			if ResourceTable().create_resource(self.request.get('res3'), consumer.key):
+				self.response.out.write('has permission on res 3<br />')
+			else:
+				self.response.out.write('could not grant on res 3<br />')
+	# end handle
+# End GetConsumer class
+		
+
+# End GetConsumer class
+
+class CreateUser(webapp.RequestHandler):
+	def get(self):
+		self.handle()
+	def post(self):
+		self.handle()
+	def handle(self):
+		self.response.out.write('''
+<html>
+<body>
+<form action="/confirm_user" METHOD="POST">
+	username: <input name="username" type="text"><br />
+	password: <input name="password" type="password"><br />
+	confirm password: <input name="confirmpassword" type="password"><br />
+	email: <input name="email" type="text"><br />
+	<input type="submit">
+</form>
+</body>
+</html>
+		''')
+# End CreateUser class
+
+class ConfirmUser(webapp.RequestHandler):
+	def post(self):
+		username = self.request.get('username')
+		password = self.request.get('password')
+		confirmpassword = self.request.get('confirmpassword')
+		email = self.request.get('email')
+
+		if not username or not password or not confirmpassword:
+			self.response.set_status(401, 'Missing field')
+			logging.error('Missing field')
+			return
+		if password != confirmpassword:
+			self.response.set_status(401, 'Password mismatch')
+			logging.error('Password mismatch')
+			return
+
+		if not UserTable().create_user(username, password, email):
+			self.response.set_status(401, 'could not create user')
+			logging.error('could not create user')
+			return
+
+		self.response.out.write('user added')
+# End ConfirmUser class
+
+class test(webapp.RequestHandler):
+	def get(self):
+		self.handle()
+	def post(self):
+		self.handle()
+	def handle(self):
+		self.response.out.write('hello')
+# End test class
+
+def main():
+	logging.getLogger().setLevel(logging.DEBUG)
+	application = webapp.WSGIApplication([('/request_token', RequestTokenHandler),
+										('/authorize', UserAuthorize),
+										('/access_token', AccessTokenHandler),
+										('/some_resource', ProtectedResourceHandler),
+										('/some_resource2', ProtectedResource2Handler),
+										('/some_resource3', ProtectedResource3Handler),
+										('/create_consumer', CreateConsumer),
+										('/get_consumer', GetConsumer),
+										('/create_user', CreateUser),
+										('/confirm_user', ConfirmUser),
+										('/', test)],
+									debug=True)
+	util.run_wsgi_app(application)
+# end main
+
+if __name__ == '__main__':
+	main()
